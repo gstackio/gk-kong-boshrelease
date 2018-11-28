1e9d49f5309e6fc0acd27817e429e097ddc0dbdf
Benjamin Gandon <benjamin@gstack.io>
2018-11-28 17:05:31 +0100

    Rebase config file on top of default config from Kong v0.14.1

---
+++ scripts/compare-conf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+RELEASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+readonly RELEASE_DIR
+
+readonly KONG_VERSION="0.14.1"
+readonly DEFAULT_CONFIG="${RELEASE_DIR}/tmp/kong-${KONG_VERSION}-default.conf"
+
+if [[ ! -f "${DEFAULT_CONFIG}" ]]; then
+    curl -L \
+        --url "https://github.com/Kong/kong/raw/${KONG_VERSION}/kong.conf.default" \
+        --output "${DEFAULT_CONFIG}"
+fi
+
+colordiff -u \
+        "${DEFAULT_CONFIG}" \
+        "${RELEASE_DIR}/jobs/kong/templates/config/kong.conf" \
+    | less -R
