9b5cec3e5277e3886365631ad850e06e2c298cce
Benjamin Gandon <benjamin@gstack.io>
2019-11-20 15:09:20 +0100

    Implement smoke tests for the 'redirect' plugin + Fix issue with 'location' header + Fix issues with the 'conf.append_query_string_to_location' feature + Fix missing 'message' in response body

--- src/plugins/redirect/handler.lua
+++ src/plugins/redirect/handler.lua
@@ -37,18 +37,12 @@ end
 function RedirectHandler:access(conf)
   RedirectHandler.super.access(self)
 
+  local status       = conf.status_code
   local content      = conf.body
   local content_type = conf.content_type
 
   if not content then
-    local encoded, err
-    encoded, err = cjson.encode({ message = conf.message or DEFAULT_RESPONSE[status] })
-    if encoded then
-      content = encoded
-    else
-      kong.log.err("could not encode value: ", err)
-      content = ""
-    end
+    content = { ["message"] = conf.message or DEFAULT_RESPONSE[status] }
     content_type = nil -- reset to the default 'application/json'
   end
 
@@ -57,27 +51,32 @@ function RedirectHandler:access(conf)
   end
 
   local headers = {
-    ["Content-Type"] = conf.content_type,
+    ["Content-Type"] = content_type,
   }
 
   if singletons.configuration.enabled_headers[constants.HEADERS.SERVER] then
     headers[constants.HEADERS.SERVER] = server_header
   end
 
-  local location       = conf.location
-  local append_req_uri = conf.append_request_uri_to_location
-  local append_qs      = conf.query_string_to_location
+  local location = conf.location
 
   if location then
-    if append_req_uri then
+    if conf.append_request_uri_to_location then
       location = location .. kong.request.get_path()
-    elseif append_qs then
+    end
+    if conf.append_query_string_to_location then
       location = location .. "?" .. kong.request.get_raw_query()
     end
-    header["Location"] = location
+    headers["Location"] = location
   end
 
-  return kong.response.exit(conf.status_code, content, headers)
+  -- kong.log.debug("Nginx request URI: [", kong.request.get_path(), "]")
+  -- kong.log.debug("Nginx query string: [", kong.request.get_raw_query(), "]")
+  -- kong.log.debug("Computed content: [", content, "]")
+  -- kong.log.debug("Configured location: [", location, "]")
+  -- kong.log.debug("Computed response headers: [", headers, "]")
+
+  return kong.response.exit(status, content, headers)
 end
 
 
